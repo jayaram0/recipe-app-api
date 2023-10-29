6b2d59e2c7c558c0ab2291193757bd9e3338893c
jayaram0 <jayaramjay000@gmail.com>
2023-10-29 13:40:49 +0530

    All changes up to documentation

---
+++ app/core/models.py
@@ -0,0 +1,47 @@
+"""
+Database Models
+Author: Jayaram
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Managers  for Users"""
+
+    def create_user(self, email, password=None, **extra_field):
+        if not email:
+            raise ValueError("User must have an email address.")
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User in the system
+    """
+
+    email = models.EmailField(max_length=150, unique=True)
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
